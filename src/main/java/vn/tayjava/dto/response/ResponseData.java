8e5b2e85d8a6c8e745bcb2ad114b43b1982ca113
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2026-01-07 11:07:27 +0700

    update

--- src/main/java/vn/tayjava/dto/response/ResponseData.java
+++ src/main/java/vn/tayjava/dto/response/ResponseData.java
@@ -12,23 +12,14 @@ public class ResponseData<T> implements Serializable {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T data;
 
-    /**
-     * Response data for the API to retrieve data successfully. For GET, POST only
-     * @param status
-     * @param message
-     * @param data
-     */
+    // GET, POST
     public ResponseData(int status, String message, T data) {
         this.status = status;
         this.message = message;
         this.data = data;
     }
 
-    /**
-     * Response data when the API executes successfully or getting error. For PUT, PATCH, DELETE
-     * @param status
-     * @param message
-     */
+    //PUT, PATCH, DELETE
     public ResponseData(int status, String message) {
         this.status = status;
         this.message = message;
